5b71f61b38f7fdd2b4fda684ee1f5f56322d582d
Javier Fuentes <javier.fuentes@hablapps.com>
2017-10-31 11:28:57 +0100

    Removed PuretestError appearances in main.

---
+++ examples/tictactoe/src/test/scala/scalatest/pure/BoardState.scala
@@ -0,0 +1,48 @@
+package org.hablapps.puretest
+package examples.tictactoe
+package test
+package pure
+
+import TicTacToe._
+
+import cats.{MonadError, ~>}
+import cats.data.StateT
+import cats.instances.either._
+
+object BoardState {
+
+  /* Auxiliary types */
+
+  type Program[A] = StateT[Either[PuretestError[Error], ?], BoardState, A]
+
+  /* Auxiliary values */
+
+  type Inner[A] = examples.tictactoe.BoardState.Program[A]
+  val Inner = examples.tictactoe.BoardState.Instance
+  val nat = new (Inner ~> Program) {
+    def apply[A](ia: Inner[A]): Program[A] =
+      StateT[Either[PuretestError[Error], ?], BoardState, A] { board =>
+        val eitherA = ia.run(board)
+        eitherA.fold(
+          e => Left(ApplicationError(e)),
+          a => Right(a))
+      }
+  }
+
+  /* Instance */
+
+  object Instance extends TicTacToe[Program] {
+
+    /* Evidences */
+    val ME: MonadError[Program, Error] = PuretestError.toMonadError
+
+    /* Transformers */
+    def reset: Program[Unit] = nat(Inner.reset)
+    def place(stone: Stone, position: Position): Program[Unit] = nat(Inner.place(stone, position))
+
+    /* Observers */
+    def in(position: Position): Program[Option[Stone]] = nat(Inner.in(position))
+    def turn: Program[Option[Stone]] = nat(Inner.turn)
+    def win(stone: Stone): Program[Boolean] = nat(Inner.win(stone))
+  }
+}
--- examples/tictactoe/src/test/scala/scalatest/pure/TicTacToeScalatestSpec.scala
+++ examples/tictactoe/src/test/scala/scalatest/pure/TicTacToeScalatestSpec.scala
@@ -7,12 +7,13 @@ import scalatestImpl.ScalatestFunSpec
 import cats.instances.either._
 
 import BoardState.Program
+import examples.tictactoe.BoardState.{empty => emptyBoard}
 
 class TicTacToeSpec extends ScalatestFunSpec[Program, TicTacToe.Error]
     with test.TicTacToeSpec[Program] {
 
   val ticTacToe = BoardState.Instance
-  val Tester = StateTester[Program, BoardState, PuretestError[TicTacToe.Error]].apply(BoardState.empty)
+  val Tester = StateTester[Program, BoardState, PuretestError[TicTacToe.Error]].apply(emptyBoard)
   val RE = RaiseError[Program, PuretestError[TicTacToe.Error]]
 
 }
